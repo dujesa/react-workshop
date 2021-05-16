9fbfcdd1c50a27b4cf148ec5fb48f57f74848bfb
dujesaric <dsaric02@fesb.hr>
2021-05-16 13:00:20 +0200

    Fixed missing import for styles.

--- src/features/restaurant/RestaurantList/RestaurantList.js
+++ src/features/restaurant/RestaurantList/RestaurantList.js
@@ -1,14 +1,16 @@
 import RestaurantListItem from "../RestaurantListItem";
+import "./RestaurantList.css";
 
-const RestaurantList = ({restaurants}) => {
+const RestaurantList = ({ restaurants }) => {
   return (
     <div className="restaurant-list-container">
-      {restaurants && restaurants.map((restaurant) => (
-        <RestaurantListItem
-          className="restaurant-list-item"
-          restaurant={restaurant}
-        />
-      ))}
+      {restaurants &&
+        restaurants.map((restaurant) => (
+          <RestaurantListItem
+            className="restaurant-list-item"
+            restaurant={restaurant}
+          />
+        ))}
     </div>
   );
 };
