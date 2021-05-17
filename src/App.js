7858f765575c624dc567467377452f092611b6bf
dujesaric <dsaric02@fesb.hr>
2021-05-17 18:23:16 +0200

    Added routes and basic redirect to 404 page.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import "./App.css";
 import Navigation from "./components/Navigation";
+import RestaurantSearchPage from "./pages/RestaurantSearchPage";
 
 const App = () => {
   return (
     <Router>
       <Navigation />
+      <Switch>
+        <Route path="/restaurants">
+          <div>Restaurant list page</div>
+        </Route>
+        <Route exact path="/">
+          <RestaurantSearchPage />
+        </Route>
+        <Route path="/404">
+          <div>not found page</div>
+        </Route>
+        <Redirect to="/404" />
+      </Switch>
     </Router>
   );
 };
