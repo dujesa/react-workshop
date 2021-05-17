dee243668fd306d514e8634f4552246fa602469a
dujesaric <dsaric02@fesb.hr>
2021-05-17 17:20:33 +0200

    Implemented favorite restaurants feature.

--- src/pages/RestaurantSearchPage/RestaurantSearchPage.js
+++ src/pages/RestaurantSearchPage/RestaurantSearchPage.js
@@ -3,27 +3,37 @@ import restaurantApi from "../../api/restaurant";
 
 import Layout from "../../components/Layout";
 import Search from "../../components/Search";
+import useFavoriteRestaurants from "../../features/restaurant/hooks/useFavoriteRestaurants";
 import RestaurantList from "../../features/restaurant/RestaurantList";
 import "./RestaurantSearchPage.css";
 
 const RestaurantSearchPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [restaurants, setRestaurants] = useState([]);
+  const { favoriteRestaurants, toggleFavorite, isFavorite } =
+    useFavoriteRestaurants();
 
   useEffect(() => {
     if (!searchTerm) {
-    } else {
-      restaurantApi.searchRestaurants(searchTerm).then(({ data }) => {
-        setRestaurants(data);
-      });
+      setRestaurants(favoriteRestaurants);
+
+      return;
     }
-  }, [searchTerm]);
+
+    restaurantApi.searchRestaurants(searchTerm).then(({ data }) => {
+      setRestaurants(data);
+    });
+  }, [favoriteRestaurants, searchTerm]);
 
   return (
     <Layout>
       <Search handleChange={setSearchTerm} />
       {searchTerm}
-      <RestaurantList restaurants={restaurants} />
+      <RestaurantList
+        restaurants={restaurants}
+        isFavorite={isFavorite}
+        toggleFavorite={toggleFavorite}
+      />
     </Layout>
   );
 };
