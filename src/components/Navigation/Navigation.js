af1cfa33c5169748b2bbb53f8d650daa609229ad
dujesaric <dsaric02@fesb.hr>
2021-05-18 18:34:22 +0200

    Navigation component finished.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,32 @@
+import { Link, useLocation } from "react-router-dom";
+
+import "./Navigation.css";
+
 const Navigation = () => {
-  return <div>navigation</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <nav className="navigation">
+      <ul className="navigation-link-wrap">
+        <li
+          className={
+            pathname === "/" ? "navigation-link--primary" : "navigation-link"
+          }
+        >
+          <Link to="/">Home</Link>
+        </li>
+        <li
+          className={
+            pathname === "/restaurants"
+              ? "navigation-link--primary"
+              : "navigation-link"
+          }
+        >
+          <Link to="/restaurants">Restaurants</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
